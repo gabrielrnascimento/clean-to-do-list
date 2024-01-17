4c3997fec83cfbcad9ab097185936570a8b32d62
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-17 19:42:17 -0300

    chore: update global CSS and Tailwind config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,13 +8,13 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            colors: {
+                primary: "#ffffff",
+                secondary: "#000000",
             },
         },
     },
     plugins: [],
 };
+
 export default config;
