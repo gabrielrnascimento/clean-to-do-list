530d67a41838eee78e81fba9aa2fa09c1b199e2c
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-20 16:37:05 -0300

    chore: update eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,9 +3,10 @@ module.exports = {
         "standard-with-typescript",
         "next/core-web-vitals",
         "plugin:prettier/recommended",
+        "plugin:react/recommended",
         "prettier",
     ],
-    plugins: ["prettier"],
+    plugins: ["react", "prettier"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         project: "./tsconfig.json",
