1babff32fdfeb6e83089ae85436eaac2baa73b96
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-19 21:55:26 -0300

    feat: ensure ToDoList component add a to do

--- frontend/src/components/to-do-list.tsx
+++ frontend/src/components/to-do-list.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import { useState, type MouseEventHandler } from "react";
+
 export const ToDoList = (): JSX.Element => {
+    const [todos, setTodos] = useState<string[]>([]);
+
+    const onClick: MouseEventHandler<HTMLButtonElement> = () => {
+        setTodos([...todos, "new todo"]);
+    };
+
     return (
         <>
             <h1 data-testid="title">things to do</h1>
-            <button data-testid="add-button">add to do</button>
+            {todos.map((todo, key) => (
+                <div data-testid="todo" key={key}>
+                    {todo}
+                </div>
+            ))}
+            <button data-testid="add-button" onClick={onClick}>
+                add to do
+            </button>
         </>
     );
 };
