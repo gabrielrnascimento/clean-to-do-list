814ce4e6193b187ed2339f97b93be724069461d5
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-19 21:55:38 -0300

    test: ensure ToDoList component add a to do

--- frontend/tests/to-do-list.test.tsx
+++ frontend/tests/to-do-list.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ToDoList } from "@/components/to-do-list";
 
 describe("ToDoList", () => {
@@ -11,5 +11,18 @@ describe("ToDoList", () => {
 
         expect(title.textContent).toBe("things to do");
         expect(addButton.textContent).toBe("add to do");
+    });
+
+    test("should add a todo", () => {
+        render(<ToDoList />);
+
+        const addButton = screen.getByTestId("add-button");
+
+        fireEvent.click(addButton);
+
+        const todo = screen.getAllByTestId("todo");
+
+        expect(todo.length).toBe(1);
+        expect(todo[0].textContent).toBe("new todo");
     });
 });
