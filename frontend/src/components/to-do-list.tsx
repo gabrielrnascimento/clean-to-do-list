7e90816c0679fdef4600494b6a51d082b0f0e85f
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-19 22:56:41 -0300

    feat: ensure ToDoList component edits a to do text content

--- frontend/src/components/to-do-list.tsx
+++ frontend/src/components/to-do-list.tsx
@@ -1,25 +1,66 @@
 "use client";
 
-import { useState, type MouseEventHandler } from "react";
+import { useState, type MouseEventHandler, useEffect } from "react";
 
 export const ToDoList = (): JSX.Element => {
     const [todos, setTodos] = useState<string[]>([]);
 
-    const onClick: MouseEventHandler<HTMLButtonElement> = () => {
+    const onAddTodo: MouseEventHandler<HTMLButtonElement> = () => {
         setTodos([...todos, "new todo"]);
+    };
+
+    const onTodoChange = (index: number, content: string): void => {
+        setTodos((prevTodos) => {
+            const newTodos = [...prevTodos];
+            newTodos[index] = content;
+            return newTodos;
+        });
     };
 
     return (
         <>
             <h1 data-testid="title">things to do</h1>
             {todos.map((todo, key) => (
-                <div data-testid="todo" key={key}>
-                    {todo}
-                </div>
+                <Todo
+                    key={key}
+                    content={todo}
+                    onChange={(content) => {
+                        onTodoChange(key, content);
+                    }}
+                />
             ))}
-            <button data-testid="add-button" onClick={onClick}>
+            <button data-testid="add-button" onClick={onAddTodo}>
                 add to do
             </button>
         </>
+    );
+};
+
+interface TodoProps {
+    content: string;
+    onChange: (content: string) => void;
+}
+
+const Todo = ({ content, onChange }: TodoProps): JSX.Element => {
+    const [todoContent, setTodoContent] = useState<string>(content);
+
+    useEffect(() => {
+        setTodoContent(content);
+    }, [content]);
+
+    const handleBlur = (): void => {
+        onChange(todoContent);
+    };
+
+    return (
+        <input
+            data-testid="todo"
+            type="text"
+            value={todoContent}
+            onBlur={handleBlur}
+            onChange={(event) => {
+                setTodoContent(event.target.value);
+            }}
+        />
     );
 };
