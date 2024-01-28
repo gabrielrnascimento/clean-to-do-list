462836efdc4ad4be20099e8f1a9cbe720a44e3ac
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 13:32:43 -0300

    feat: ensure ToDoItem component displays correct initial values

---
+++ src/components/to-do-item.tsx
@@ -0,0 +1,9 @@
+export const ToDoItem = (): JSX.Element => {
+    return (
+        <>
+            <input type="checkbox" />
+            <input type="text" placeholder={"new to-do"} />
+            <button>delete to-do</button>
+        </>
+    );
+};
