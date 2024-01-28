555711a3c638ea189e8e9a2a9d8e476edd2bc388
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 12:46:08 -0300

    feat: ensure ToDoList component displays correct initial content

---
+++ src/components/to-do-list.tsx
@@ -0,0 +1,8 @@
+export const ToDoList = (): JSX.Element => {
+    return (
+        <>
+            <h1 data-testid="title">things to do</h1>
+            <button data-testid="add-button">add to-do</button>
+        </>
+    );
+};
