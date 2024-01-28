2f51f93d0ce41c546ade9b566dc39356021bdfb4
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 12:49:18 -0300

    refactor: move SUT creation to a factory method

--- tests/components/to-do-list.spec.tsx
+++ tests/components/to-do-list.spec.tsx
@@ -1,10 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { ToDoList } from "../../src/components/to-do-list";
 
+const makeSut = (): void => {
+    render(<ToDoList />);
+};
+
 describe("ToDoList", () => {
     test("should display correct initial content", () => {
-        render(<ToDoList />);
-
+        makeSut();
         const title = screen.getByRole("heading", { name: "things to do" });
         const addButton = screen.getByRole("button", { name: "add to-do" });
 
