7aa58acdf9ea1ee6b86901ae9a2d46212282be35
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 15:45:27 -0300

    feat: ensure ResizableInputText has correct styles for resizable components

--- src/components/resizable-input-text.tsx
+++ src/components/resizable-input-text.tsx
@@ -1,4 +1,39 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const ResizableContainer = styled.div`
+    position: relative;
+    display: inline-block;
+`;
+
+const ResizableText = styled.span`
+    margin: 0;
+    padding: 0 0.4rem 0 0.2rem;
+    font-size: inherit;
+    font-family: sans-serif;
+
+    display: inline-block;
+    visibility: hidden;
+    white-space: pre;
+`;
+
+const ResizableInput = styled.input`
+    margin: 0;
+    padding: 0 0.2rem;
+    font-size: inherit;
+    font-family: sans-serif;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    background-color: transparent;
+    outline: none;
+    color: #fff;
+    border: none;
+`;
 
 type ResizableInputProps = {
     placeholder: string;
@@ -16,11 +51,11 @@ export const ResizableInputText = ({
     };
 
     return (
-        <>
-            <span data-testid="resizable-text">
+        <ResizableContainer data-testid="resizable-container">
+            <ResizableText data-testid="resizable-text">
                 {inputValue !== "" ? inputValue : placeholder}
-            </span>
-            <input
+            </ResizableText>
+            <ResizableInput
                 data-testid="resizable-input"
                 type="text"
                 placeholder={placeholder}
@@ -28,6 +63,6 @@ export const ResizableInputText = ({
                 onChange={handleInputChange}
                 autoFocus
             />
-        </>
+        </ResizableContainer>
     );
 };
