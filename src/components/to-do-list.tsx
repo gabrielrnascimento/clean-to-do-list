053bc70afb7f47c726ed02168701ca8b2155cb8a
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 13:10:58 -0300

    feat: ensure ToDoList component adds a to-do on button click with correct values

--- src/components/to-do-list.tsx
+++ src/components/to-do-list.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 export const ToDoList = (): JSX.Element => {
+    const [toDos, setToDos] = useState<string[]>([]);
+
+    const onClick = (): void => {
+        setToDos([...toDos, "new to-do"]);
+    };
+
     return (
         <>
             <h1>things to do</h1>
-            <button>add to-do</button>
+            <ul>
+                {toDos.map((toDo, key) => (
+                    <li key={key}>{toDo}</li>
+                ))}
+            </ul>
+            <button onClick={onClick}>add to-do</button>
         </>
     );
 };
