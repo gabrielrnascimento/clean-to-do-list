7c8a271c7f666091a52a51b3556a5fa06d5c7c05
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-01-28 13:11:07 -0300

    test: ensure ToDoList component adds a to-do on button click with correct values

--- tests/components/to-do-list.spec.tsx
+++ tests/components/to-do-list.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { ToDoList } from "../../src/components/to-do-list";
 
 const makeSut = (): void => {
@@ -6,12 +6,25 @@ const makeSut = (): void => {
 };
 
 describe("ToDoList", () => {
-    test("should display correct initial content", () => {
+    test("should display correct initial values", () => {
         makeSut();
         const title = screen.getByRole("heading", { name: "things to do" });
         const addButton = screen.getByRole("button", { name: "add to-do" });
 
         expect(title.textContent).toBe("things to do");
         expect(addButton.textContent).toBe("add to-do");
+    });
+
+    test("should add a to-do with correct values ", () => {
+        makeSut();
+        const addButton = screen.getByRole("button", { name: "add to-do" });
+
+        fireEvent.click(addButton);
+        fireEvent.click(addButton);
+        const toDos = screen.getAllByRole("listitem");
+
+        expect(toDos.length).toBe(2);
+        expect(toDos[0].textContent).toBe("new to-do");
+        expect(toDos[1].textContent).toBe("new to-do");
     });
 });
