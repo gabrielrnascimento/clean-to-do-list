3be0275ab3a12e5b76066c4bdd5b0986e6b4c609
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-04-22 20:19:33 -0300

    feat: add provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { container } from "./@core/infra/dependendy-injection/awilix";
 import "./App.css";
+import { container } from "./@core/infra/dependendy-injection/awilix";
 import { ToDoList } from "./components/to-do-list";
+import { ToDoProvider } from "./contexts/to-do";
 
 function App(): JSX.Element {
     return (
-        <ToDoList
+        <ToDoProvider
             listToDosUseCase={container.resolve("remoteListToDosUseCase")}
-        />
+            createToDoUseCase={container.resolve("remoteCreateToDoUseCase")}
+            updateToDoUseCase={container.resolve("remoteUpdateToDoUseCase")}
+            deleteToDoUseCase={container.resolve("remoteDeleteToDoUseCase")}
+        >
+            <ToDoList />
+        </ToDoProvider>
     );
 }
 
--- src/components/to-do-list.tsx
+++ src/components/to-do-list.tsx
@@ -1,74 +1,46 @@
-import { useEffect, useState } from "react";
 import { ToDoItem } from "./to-do-item";
-import { type ListToDosUseCase } from "../@core/domain/usecases/list-to-dos.usecase";
-import { ToDo } from "../@core/domain/entities";
+import { useToDoContext } from "../contexts/to-do";
 
-type Props = {
-    listToDosUseCase: ListToDosUseCase;
-};
-
-export const ToDoList = ({ listToDosUseCase }: Props): JSX.Element => {
-    const [toDos, setToDos] = useState<ToDo[]>([]);
-
-    const handleAddToDo = (): void => {
-        setToDos([...toDos, new ToDo({ isDone: false, description: "" })]);
-    };
-
-    const handleToDoDescriptionChange = (
-        index: number,
-        description: string
-    ): void => {
-        const newToDos = [...toDos];
-        newToDos[index].description = description;
-        setToDos(newToDos);
-    };
-
-    const handleDeleteToDo = (index: number): void => {
-        const newToDos = [...toDos];
-        newToDos.splice(index, 1);
-        setToDos(newToDos);
-    };
-
-    const handleToDoStatusChange = (index: number): void => {
-        const newToDos = [...toDos];
-        newToDos[index].isDone = !newToDos[index].isDone;
-        setToDos(newToDos);
-    };
-
-    useEffect(() => {
-        listToDosUseCase
-            .listToDos()
-            .then((response) => {
-                setToDos(response);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+export const ToDoList = (): JSX.Element => {
+    const {
+        toDos,
+        handleAddToDo,
+        handleDeleteToDo,
+        handleToDoDescriptionChange,
+        handleToDoStatusChange,
+    } = useToDoContext();
 
     return (
         <>
             <h1>things to do</h1>
             <ul>
-                {toDos.map((todo, key) => (
+                {toDos.map((toDo) => (
                     <ToDoItem
-                        key={key}
-                        description={todo.description}
-                        isDone={todo.isDone}
+                        key={toDo.id}
+                        description={toDo.description}
+                        isDone={toDo.isDone}
                         onDescriptionChange={(newDescription: string) => {
-                            handleToDoDescriptionChange(key, newDescription);
+                            void handleToDoDescriptionChange(
+                                toDo.id,
+                                newDescription
+                            );
                         }}
                         onDelete={() => {
-                            handleDeleteToDo(key);
+                            void handleDeleteToDo(toDo.id);
                         }}
                         onStatusChange={() => {
-                            handleToDoStatusChange(key);
+                            void handleToDoStatusChange(toDo.id, !toDo.isDone);
                         }}
                     />
                 ))}
             </ul>
-            <button onClick={handleAddToDo}>add to-do</button>
+            <button
+                onClick={() => {
+                    void handleAddToDo();
+                }}
+            >
+                add to-do
+            </button>
         </>
     );
 };
