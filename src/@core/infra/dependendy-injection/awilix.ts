5e31475a6b28ad7100b74f1f8d285a6d1d7ae9a7
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-04-22 20:07:12 -0300

    feat: register new dependencies to awilix container

--- src/@core/infra/dependendy-injection/awilix.ts
+++ src/@core/infra/dependendy-injection/awilix.ts
@@ -1,6 +1,11 @@
 import { createContainer, asClass, asValue } from "awilix";
 import { AxiosHttpClient } from "../http/axios-http-client";
-import { RemoteListToDosUseCase } from "../../application/to-do";
+import {
+    RemoteCreateToDoUseCase,
+    RemoteDeleteToDoUseCase,
+    RemoteListToDosUseCase,
+    RemoteUpdateToDoUseCase,
+} from "../../application/to-do";
 import { ToDoHttpGateway } from "../gateways/to-do-http.gateway";
 import { env } from "../config";
 
@@ -11,7 +16,15 @@ export const container = createContainer({
 
 container.register({
     httpClient: asClass(AxiosHttpClient),
-    listToDosGateway: asClass(ToDoHttpGateway),
     url: asValue(env.apiUrl),
+
+    listToDosGateway: asClass(ToDoHttpGateway),
+    createToDoGateway: asClass(ToDoHttpGateway),
+    updateToDoGateway: asClass(ToDoHttpGateway),
+    deleteToDoGateway: asClass(ToDoHttpGateway),
+
     remoteListToDosUseCase: asClass(RemoteListToDosUseCase),
+    remoteCreateToDoUseCase: asClass(RemoteCreateToDoUseCase),
+    remoteUpdateToDoUseCase: asClass(RemoteUpdateToDoUseCase),
+    remoteDeleteToDoUseCase: asClass(RemoteDeleteToDoUseCase),
 });
