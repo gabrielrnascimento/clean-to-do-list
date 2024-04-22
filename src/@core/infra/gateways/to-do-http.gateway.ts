3743e0b793f9ac6aee46449dd35fa65fdd738e95
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-04-22 19:57:49 -0300

    feat: implement ToDoGateway interfaces

--- src/@core/infra/gateways/to-do-http.gateway.ts
+++ src/@core/infra/gateways/to-do-http.gateway.ts
@@ -1,11 +1,24 @@
 import { HttpMethod, type HttpClient } from "../../application/http";
 import { type ToDo } from "../../domain/entities";
-import { type ListToDosGateway } from "../../domain/gateways/to-do";
+import {
+    type CreateToDoGatewayParams,
+    type CreateToDoGateway,
+    type ListToDosGateway,
+    type DeleteToDoGateway,
+    type UpdateToDoGateway,
+    type UpdateToDoGatewayParams,
+} from "../../domain/gateways/to-do";
 
-export class ToDoHttpGateway implements ListToDosGateway {
+export class ToDoHttpGateway
+    implements
+        ListToDosGateway,
+        CreateToDoGateway,
+        DeleteToDoGateway,
+        UpdateToDoGateway
+{
     constructor(
         private readonly url: string,
-        private readonly httpClient: HttpClient<ToDo[]>
+        private readonly httpClient: HttpClient<Partial<ToDo>, ToDo[]>
     ) {}
 
     async getAll(): Promise<ToDo[]> {
@@ -15,5 +28,34 @@ export class ToDoHttpGateway implements ListToDosGateway {
         });
 
         return response.body ?? [];
+    }
+
+    async create(params: CreateToDoGatewayParams): Promise<void> {
+        await this.httpClient.request({
+            method: HttpMethod.POST,
+            url: this.url,
+            body: {
+                description: params.description,
+                isDone: false,
+            },
+        });
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.httpClient.request({
+            method: HttpMethod.DELETE,
+            url: `${this.url}/${id}`,
+        });
+    }
+
+    async update(params: UpdateToDoGatewayParams): Promise<void> {
+        await this.httpClient.request({
+            method: HttpMethod.PATCH,
+            url: `${this.url}/${params.id}`,
+            body: {
+                description: params.description,
+                isDone: params.isDone,
+            },
+        });
     }
 }
