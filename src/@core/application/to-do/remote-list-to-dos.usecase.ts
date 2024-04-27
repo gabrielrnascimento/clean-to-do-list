cb3a851e91ad584ce94c555dc24cc264ab703f06
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-04-27 00:58:44 -0300

    refactor: implement ListToDosUseCase interface

--- src/@core/application/to-do/remote-list-to-dos.usecase.ts
+++ src/@core/application/to-do/remote-list-to-dos.usecase.ts
@@ -1,7 +1,8 @@
 import { type ToDo } from "../../domain/entities";
 import { type ListToDosGateway } from "../../domain/gateways/to-do";
+import { type ListToDosUseCase } from "../../domain/usecases";
 
-export class RemoteListToDosUseCase {
+export class RemoteListToDosUseCase implements ListToDosUseCase {
     constructor(private readonly listToDosGateway: ListToDosGateway) {}
 
     async listToDos(): Promise<ToDo[]> {
