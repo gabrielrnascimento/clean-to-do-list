f8f8e7fcafe344e50d7fe34e9357340c719a51db
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2024-04-28 21:48:47 -0300

    feat: update ToDoList component styles

--- src/components/to-do-list.tsx
+++ src/components/to-do-list.tsx
@@ -2,6 +2,12 @@ import { ToDoItem } from "./to-do-item";
 import { useToDoContext } from "../contexts/to-do";
 import styled from "styled-components";
 
+const Title = styled.h1`
+    font-size: 4rem;
+    text-align: center;
+    color: #fff;
+`;
+
 const AddButton = styled.button`
     background-image: url("../../public/plus-regular.svg");
     background-size: cover;
@@ -10,19 +16,14 @@ const AddButton = styled.button`
     width: 2rem;
     background-color: transparent;
     border: none;
-
     cursor: pointer;
+    align-self: center;
 `;
 
 const ToDoListContainer = styled.div`
     font-size: 2rem;
     display: flex;
     flex-direction: column;
-    align-items: center;
-`;
-
-const UnorderedList = styled.ul`
-    padding: 0;
 `;
 
 export const ToDoList = (): JSX.Element => {
@@ -30,17 +31,15 @@ export const ToDoList = (): JSX.Element => {
 
     return (
         <ToDoListContainer>
-            <h1>things to do</h1>
-            <UnorderedList>
-                {toDos.map((toDo) => (
-                    <ToDoItem
-                        key={toDo.id}
-                        id={toDo.id}
-                        description={toDo.description}
-                        isDone={toDo.isDone}
-                    />
-                ))}
-            </UnorderedList>
+            <Title>things to do</Title>
+            {toDos.map((toDo) => (
+                <ToDoItem
+                    key={toDo.id}
+                    id={toDo.id}
+                    description={toDo.description}
+                    isDone={toDo.isDone}
+                />
+            ))}
             <AddButton
                 title="create to do"
                 data-testid="add-button"
